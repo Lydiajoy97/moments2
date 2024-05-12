81ac2c5a3c8f4a3f7cef6ba2c0ae7eac7090f958
Lydia Young <lydsjoyy@gmail.com>
2024-05-12 16:31:11 +0000

    sign in pages commit

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL  = "https://app-moments-499187305603.herokuapp.com/"
+axious.defaults.headers.post['Content-Type'] = 'mulitpart/form-data'
+axious.defaults.withCredentials = true 
