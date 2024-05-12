5926de674b98760103aed9496822437d55854ec1
Lydia Young <lydsjoyy@gmail.com>
2024-05-12 18:18:47 +0000

    use context hooks

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 axios.defaults.baseURL  = "https://app-moments-499187305603.herokuapp.com/"
-axious.defaults.headers.post['Content-Type'] = 'mulitpart/form-data'
-axious.defaults.withCredentials = true 
+axios.defaults.headers.post['Content-Type'] = 'mulitpart/form-data'
+axios.defaults.withCredentials = true 
